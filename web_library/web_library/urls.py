01c54fd96a608596664f472d4ab9381d877421dc
Forn <belan.artem2012@gmail.com>
2021-12-07 17:08:24 +0200

    add "django.conf.urls.static", "django.views.generic.RedirectView" to "catalog"  (step №5)

--- web_library/web_library/urls.py
+++ web_library/web_library/urls.py
@@ -23,11 +23,20 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include(('catalog.urls', 'catalog'))),
+    # path('catalog/', include(('catalog.urls', 'catalog'))),
+    path('catalog/', include('catalog.urls')),
 
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
 #  static_file
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
