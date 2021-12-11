3c9528d53f3df2a7e9e8415ff8858c19726a7cf1
Forn <belan.artem2012@gmail.com>
2021-12-11 17:01:38 +0200

    create path 'book-detail', 'author-detail', 'books-list'

--- web_library/catalog/urls.py
+++ web_library/catalog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls import url
+
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books-list'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    # path('Record/', views.RecordDetailView.as_view(), name='RecordDetailView'),
 ]
