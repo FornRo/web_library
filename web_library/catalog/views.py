50a6a3240d06ce0fe85e8590e802f22c23396658
Forn <belan.artem2012@gmail.com>
2021-12-14 01:56:52 +0200

    add decorator for restrict access to the funcks '@permission_required'

--- web_library/catalog/views.py
+++ web_library/catalog/views.py
@@ -63,6 +63,7 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
 # ________________________________________________________________________
 
+from django.contrib.auth.decorators import permission_required
 
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
@@ -72,26 +73,29 @@ import datetime
 from .forms import RenewBookForm
 
 
+@permission_required('catalog.can_mark_returned')
 def renew_book_librarian(request, pk):
+    """
+    View function for renewing a specific BookInstance by librarian
+    """
     book_inst = get_object_or_404(models.BookInstance, pk=pk)
 
-    # Если данный запрос типа POST, тогда
+    # If this is a POST request then process the Form data
     if request.method == 'POST':
 
-        # Создаём экземпляр формы и заполняем данными из запроса (связывание, binding):
+        # Create a form instance and populate it with data from the request (binding):
         form = RenewBookForm(request.POST)
 
-        # Проверка валидности данных формы:
+        # Check if the form is valid:
         if form.is_valid():
-            # Обработка данных из form.cleaned_data
-            #(здесь мы просто присваиваем их полю due_back)
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
             book_inst.due_back = form.cleaned_data['renewal_date']
             book_inst.save()
 
-            # Переход по адресу 'all-borrowed':
-            return HttpResponseRedirect(reverse('all-borrowed'))
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed') )
 
-    # Если это GET (или какой-либо ещё), создать форму по умолчанию.
+    # If this is a GET (or any other method) create the default form.
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
         form = RenewBookForm(initial={'renewal_date': proposed_renewal_date, })
