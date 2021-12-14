8b6c11f6ad9add3db4f448a272a3143d996e4885
Forn <belan.artem2012@gmail.com>
2021-12-14 03:02:22 +0200

    created display classes & editing & delite 'author'

--- web_library/catalog/urls.py
+++ web_library/catalog/urls.py
@@ -5,11 +5,20 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
+]
+
+urlpatterns += [
     url(r'^books/$', views.BookListView.as_view(), name='books-list'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+]
 
+urlpatterns += [
     path('authors/', views.AuthorListView.as_view(), name='authors-list'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
 ]
 
 urlpatterns += [
