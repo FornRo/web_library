cacc462360d6ce96469e45b5af9d30d03e1b2331
Forn <belan.artem2012@gmail.com>
2021-12-17 20:15:17 +0200

    .

--- web_library/web_library/settings.py
+++ web_library/web_library/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'cg#p$g+j9tax!#bycup@1$8obt2_+&klj+pmu)5%asj6yjpkag'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
