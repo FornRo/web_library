60ff45f11802aec347762ae426e49dece63edb32
Forn <belan.artem2012@gmail.com>
2021-12-17 16:10:04 +0200

    change 'django.conf.urls.url' to 'django.urls.re_path'

--- web_library/catalog/urls.py
+++ web_library/catalog/urls.py
@@ -1,30 +1,33 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
-from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
 
 urlpatterns += [
-    url(r'^books/$', views.BookListView.as_view(), name='books-list'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books-list'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+    re_path(r'^books/create/$', views.AuthorCreate.as_view(), name='books_create'),
+    re_path(r'^books/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='books_update'),
+    re_path(r'^books/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='books_delete'),
 ]
 
 urlpatterns += [
     path('authors/', views.AuthorListView.as_view(), name='authors-list'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 
-    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
-    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
-    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+    re_path(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    re_path(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    re_path(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
 ]
 
 urlpatterns += [
-    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
 
 urlpatterns += [
-    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    re_path(r'^book/(?P<pk>[-\w]+)/renew/$', views.RenewBookLibrarian.as_view(), name='renew-book-librarian'),
 ]
