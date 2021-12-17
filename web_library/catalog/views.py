abdadea4393c9eff2ba647613d1618caded4bb44
Forn <belan.artem2012@gmail.com>
2021-12-17 16:11:35 +0200

    change path-file 'index.html'

--- web_library/catalog/views.py
+++ web_library/catalog/views.py
@@ -19,7 +19,7 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
-        'catalog/index.html',
+        'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors,
                  'num_visits': num_visits},
