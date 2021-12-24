c299d6f45c5488ecb683b92ffc572007316d09b8
forn <belan.artem2012@gmail.com>
2021-12-24 13:44:36 +0200

    add allowed_hosts

--- catalog/management/commands/createdata.py
+++ catalog/management/commands/createdata.py
@@ -4,13 +4,11 @@ from faker import Faker
 from catalog import models
 import json
 
-LANGUAGE = ["en", "ru", "uk", ]
-
 
 class Provider(faker.providers.BaseProvider):
-    def generate_genre(self):
-        return self.
 
+    def get_random_element_(self):
+        return self.random_element([1, 2, 3, 4, 5])
 
 
 class Command(BaseCommand):
@@ -19,10 +17,58 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         fake = Faker(['en'])
         fake.add_provider(Provider)
+
+        self.generate_language()
+        self.generate_genre()
+
+
+
+        # print(fake.name())
+
+    def generate_language(self):
         # iter all language
-        for val in iter(LANGUAGE):
+        language_ = ["en", "ru", "uk", ]
+        for val in iter(language_):
             models.Language.objects.create(name=val)
         else:
+            check_genre = models.Language.objects.all().count()
+            self.stdout.write(self.style.SUCCESS(f'Number of Language {check_genre}'))
+
+    def generate_genre(self):
+        check_genre = models.Genre.objects.all().count()
+        self.stdout.write(self.style.SUCCESS(f'Number of genre {check_genre}'))
+
+        # iter all genre_for_books
+        genre_ = [
+            # List of fiction book genres:
+            'Adventure',
+            'Fantasy',
+            'Romance',
+            'Contemporary',
+            'Dystopian',
+            'Mystery',
+            'Horror',
+            'Thriller',
+            'Paranormal',
+            'Historical fiction',
+            'Science Fiction',
+            'Children’s',
+            # List of nonfiction book genres:
+            'Memoir',
+            'Cooking',
+            'Art',
+            'Self-help / Personal',
+            'Development',
+            'Motivational',
+            'Health',
+            'History',
+            'Travel',
+            'Guide / How-to',
+            'Families & Relationships',
+            'Humor',
+        ]
+        for val in iter(genre_):
+            models.Genre.objects.create(name=val)
+        else:
             check_genre = models.Genre.objects.all().count()
-            self.stdout.write(self.style.SUCCESS(f'Number of genre {check_genre}'))
-        # print(fake.name())
+            self.stdout.write(self.style.SUCCESS(f'Number of Genre {check_genre}'))
